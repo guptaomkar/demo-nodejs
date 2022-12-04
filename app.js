283dd13a59c063d66f1ac906173783f6af3731e3
omkar <omkar.gupta1999@gmail.com>
2022-12-04 14:56:18 +0530

    second

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ app.listen(3300, () => { console.log("Server started on port 3300") });
 app.get('/', (req, res) => {
     res.send("Hello world");
 })
+
+app.get('/welcome', (req, res) => {
+    res.send("welcome to jenkins");
+})
